90f95bf377c6f86de589c69d10be8a77aa9392e2
Haohan Jiang <haohan.jiang@nike.com>
2022-12-30 19:43:40 -0800

    starter code

---
+++ app/static/data/generate_students.py
@@ -0,0 +1,21 @@
+from app.static.models import student
+
+
+def generate_names():
+    pass
+
+
+def generate_address():
+    pass
+
+
+def generate_dob():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ app/static/models/student.py
@@ -0,0 +1,6 @@
+class Student:
+    def __init__(self, name, address, dob):
+        self.name = name
+        self.address = address
+        self.date_of_birth = dob
+
